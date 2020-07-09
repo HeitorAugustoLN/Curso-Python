381d1529bdf301f4d1b4ac3b906a740034b9fbfd
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 14:37:44 -0300

    ✨ Add Exercise 14

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio14.py
@@ -0,0 +1,4 @@
+temperaturaCelsius = float(input('Informe a temperatura em °C: '))
+temperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32
+
+print(f'A temperatura de {temperaturaCelsius}°C corresponde a {temperaturaFahrenheit}°F')
