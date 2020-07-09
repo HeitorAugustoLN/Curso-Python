ec7601e3be38507b5a1ecbb13a9736f0bcc601b6
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 13:22:55 -0300

    ✨ Add Exercise 7

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio07.py
@@ -0,0 +1,5 @@
+primeiraNota = float(input('Primeira nota do aluno: '))
+segundaNota = float(input('Segunda nota do aluno: '))
+media = (primeiraNota + segundaNota) / 2
+
+print(f'A média entre {primeiraNota} e {segundaNota} é {media}')
