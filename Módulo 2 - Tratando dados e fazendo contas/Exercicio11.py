76547167aa54e79f97951dce24bbde2b04d7fe11
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 14:12:29 -0300

    ✨ Add Exercise 11

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio11.py
@@ -0,0 +1,8 @@
+larguraParede = float(input('Largura da parede: '))
+alturaParede = float(input('Altura da parede: '))
+
+metrosQuadrados = larguraParede * alturaParede
+quantidadeDeTinta = metrosQuadrados / 2
+
+print(f'Sua parede tem a dimensão de {larguraParede} x {alturaParede} e sua área é de {metrosQuadrados}m².')
+print(f'Para pintar essa parede, você precisará de {quantidadeDeTinta}l de tinta.')
