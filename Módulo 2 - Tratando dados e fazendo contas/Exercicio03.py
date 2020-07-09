13d24c0216796fcaac1765bf4d50188446d14661
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-08 22:55:25 -0300

    ⚡ Refactor exercise 3

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio03.py
@@ -0,0 +1,5 @@
+primeiroNumero = int(input('Digite um número: '))
+segundoNumero = int(input('Digite outro número: '))
+soma = primeiroNumero + segundoNumero
+
+print(f'A soma entre {primeiroNumero} e {segundoNumero} resulta em {soma}')
