dd78e39f08ca793b7793f179dac780197ed68e31
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 14:02:56 -0300

    ✨ Add Exercise 10

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio10.py
@@ -0,0 +1,4 @@
+dinheiroEmReais = float(input('Quanto dinheiro você tem na carteira? R$'))
+dinheiroEmDolar = dinheiroEmReais * 0.19
+
+print(f'Com R${dinheiroEmReais} você pode comprar US${dinheiroEmDolar}')
