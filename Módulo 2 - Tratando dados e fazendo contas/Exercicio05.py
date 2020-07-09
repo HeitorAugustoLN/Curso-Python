9c5909f96cf4fe32323f1f3ec149dddb438b74f6
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 00:57:31 -0300

    ✨ Add Exercise 5

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio05.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'Analisando o valor {numero}, seu antecessor é {antecessor} e o s3eu sucessor é {sucessor}')
