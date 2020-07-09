9342056d60d2540f2f8240b78e068c8e396eb2d2
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-09 14:28:13 -0300

    ✨ Add Exercise 13

---
+++ Módulo 2 - Tratando dados e fazendo contas/Exercicio13.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual é o salário do funcionário? R$'))
+reajuste = salario + (salario * 15 / 100)
+
+print(f'Um funcionário que ganhava R${salario}, com 15% de aumento, passa a receber R${round(reajuste, 2)}')
