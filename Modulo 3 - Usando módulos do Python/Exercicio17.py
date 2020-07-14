522f9ba715a4dbb9e78560297e384f44d020079b
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-14 14:04:28 -0300

    ✨ Add Exercise 17

---
+++ Modulo 3 - Usando módulos do Python/Exercicio17.py
@@ -0,0 +1,7 @@
+from math import hypot
+
+catetoOposto = float(input("Comprimento do cateto oposto: "))
+catetoAdjacente = float(input("Comprimento do cateto adjacente: "))
+hipotenusa = hypot(catetoOposto, catetoAdjacente)
+
+print(f"A hipotenusa vai medir {round(hipotenusa, 2)}")
