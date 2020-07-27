601a6d7d87a4210b23e4cb2d2632d313f689e532
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-07-27 00:23:16 -0300

    :zap: Organize exercises by "Mundos/Modules"

--- Modulo3/Exercicio21.py
+++
@@ -1,3 +0,0 @@
-from playsound import playsound
-
-playsound("F:\Projetos\python-course\Modulo3\musica.mp3", True)
---
+++ Mundo 01/Módulo 03/Exercicio21.py
@@ -0,0 +1,5 @@
+from playsound import playsound
+
+playsound(
+    "/home/heitor/www/github/heitoraugustoln/python-course/Módulo 3/musica.mp3", True
+)
