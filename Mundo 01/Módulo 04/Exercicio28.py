0ea170316ee7cf8b4a3e862c736b74c8b482ae2d
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-04 10:41:19 -0300

    :zap: Exchange choice for randint

--- Mundo 01/Módulo 04/Exercicio28.py
+++ Mundo 01/Módulo 04/Exercicio28.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import randint
 
 print("Vou pensar em um número de 0 a 5, tente advinhar!")
 
@@ -6,8 +6,7 @@ numeroUsuario = int(input("Em que número eu pensei? "))
 
 print("PROCESSANDO...")
 
-numeros = [0, 1, 2, 3, 4, 5]
-numeroEscolhido = choice(numeros)
+numeroEscolhido = randint(0, 5)
 
 if numeroUsuario == numeroEscolhido:
   print(f"Você... ACERTOU! Eu realmente escolhi {numeroEscolhido}")
