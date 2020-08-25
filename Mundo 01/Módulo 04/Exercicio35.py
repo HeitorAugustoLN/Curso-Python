9c4bfeceb125ca3983f8f52a6831b42bd5ea150a
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-24 22:45:13 -0300

    :sparkles: Add Exercise 35

---
+++ Mundo 01/Módulo 04/Exercicio35.py
@@ -0,0 +1,8 @@
+a = float(input("Primeiro segmento: "))
+b = float(input("Segundo segmento: "))
+c = float(input("Terceiro segmento: "))
+
+if a < b + c and b < a + c and c < a + b:
+	print("Os segmentos acima PODEM FORMAR um triângulo")
+else:
+	print("Os segmentos acima NÃO PODEM FORMAR um triângulo")
