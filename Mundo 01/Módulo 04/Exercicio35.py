de024235fe5c50d5301a354bb20a70eac903b81a
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-24 23:32:53 -0300

    :fire: Remove duplicated exercise in wrong folder

--- Mundo 01/Módulo 04/Exercicio35.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Primeiro segmento: "))
-b = float(input("Segundo segmento: "))
-c = float(input("Terceiro segmento: "))
-
-if a < b + c and b < a + c and c < a + b:
-	print("Os segmentos acima PODEM FORMAR um triângulo")
-else:
-	print("Os segmentos acima NÃO PODEM FORMAR um triângulo")
