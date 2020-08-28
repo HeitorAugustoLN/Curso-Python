ce0daeb578e72b1c9b5d00f02f10abbc983a952e
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-28 17:30:34 -0300

    ✨ Add Exercise 38

---
+++ Mundo 02/Módulo 02/Exercicio38.py
@@ -0,0 +1,9 @@
+primeiroNumero = int(input("Primeiro número: "))
+segundoNumero = int(input("Segundo número: "))
+
+if primeiroNumero > segundoNumero:
+    print("O primeiro valor é maior")
+elif primeiroNumero == segundoNumero:
+    print("Os dois valores são iguais")
+else:
+    print("O segundo valor é maior")
