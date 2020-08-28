658d68d085903b34ee3de51d10a373601a3bd7db
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-28 18:52:14 -0300

    ✨ Add Exercise 39

---
+++ Mundo 02/Módulo 02/Exercicio39.py
@@ -0,0 +1,17 @@
+import datetime
+
+agora = datetime.datetime.now()
+anoAtual = agora.year
+anoNascimento = int(input("Ano de nascimento: "))
+idade = anoAtual - anoNascimento
+
+print(f"Quem nasceu em {anoNascimento} tem {idade} anos em {anoAtual}")
+
+if idade < 18:
+    print(f"Ainda faltam {(anoNascimento + 18) - anoAtual} anos para o alistamento")
+    print(f"Seu alistamento será em {anoAtual + idade}")
+elif idade > 18:
+    print(f"Você deveria ter se alistado há {anoAtual - (anoNascimento - (idade - 18))} anos")
+    print(f"Seu alistamento foi em {anoNascimento - (idade - 18)}")
+else:
+    print("Você tem que se alistar imediatamente")
