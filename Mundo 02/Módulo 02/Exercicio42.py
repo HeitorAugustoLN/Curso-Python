469942f41276845be029115e167233fdca926350
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-08-29 09:51:10 -0300

    ✨ Add Exercise 42

---
+++ Mundo 02/Módulo 02/Exercicio42.py
@@ -0,0 +1,13 @@
+a = float(input("Primeiro segmento: "))
+b = float(input("Segundo segmento: "))
+c = float(input("Terceiro segmento: "))
+
+if a < b + c and b < a + c and c < a + b:
+    if a == b == c == a:
+        print("Os segmentos podem formar um triângulo equilátero")
+    elif a != b != c != a:
+        print("Os segmentos podem formar um triângulo escaleno")
+    else:
+        print("Os segmentos podem formar um triângulo isósceles")
+else:
+    print("Os segmentos não podem formar um triângulo") 
