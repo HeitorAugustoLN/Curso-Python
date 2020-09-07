7881a042f13e1fab4b8896643d988ed04872f035
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-09-07 08:32:13 -0300

    ✨ Add Exercise 54

---
+++ Mundo 02/Módulo 03/Exercicio54.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+anoAtual = datetime.now().year
+maiores = 0
+menores = 0
+
+print("=" * 37)
+for pessoa in range(1, 8):
+    nascimento = int(input(f"Em que ano a {pessoa}ª pessoa nasceu? "))
+    if anoAtual - nascimento >= 18:
+        maiores += 1
+        pass
+    else:
+        menores += 1
+        pass
+    pass
+print("=" * 37)
+print(f"Menores de idade: {menores}")
+print(f"Maiores de idade: {maiores}")
+print("=" * 37)
