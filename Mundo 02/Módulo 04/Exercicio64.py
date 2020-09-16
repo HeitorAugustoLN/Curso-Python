c9c0bb097fb99ed0f48188a1051cb13cec68080b
Heitor Augusto <heitoraugustoln@outlook.com>
2020-09-16 09:00:14 -0300

    ✨ Add Exercise 64

---
+++ Mundo 02/Módulo 04/Exercicio64.py
@@ -0,0 +1,8 @@
+numero = contador = soma = 0
+numero = int(input("Digite um número [999 para parar]: "))
+while numero != 999:
+    contador += 1
+    soma += numero
+    numero = int(input("Digite um número [999 para parar]: "))
+
+print(f"Você digitou {contador} números e a soma entre eles foi: {soma}")
