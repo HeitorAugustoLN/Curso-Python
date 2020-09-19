c584a00fe3ba5410d2338323f04ea79c22838bd2
Heitor Augusto <heitoraugustoln@outlook.com>
2020-09-19 15:42:22 -0300

    :sparkles: Add Exercise 75

---
+++ Mundo 03/Módulo 01/Exercicio75.py
@@ -0,0 +1,24 @@
+primeiroValor = int(input("Digite um número: "))
+segundoValor = int(input("Digite outro número: "))
+terceiroValor = int(input("Digite mais um número: "))
+quartoValor = int(input("Digite o último número: "))
+
+tupla = (primeiroValor, segundoValor, terceiroValor, quartoValor)
+numerosPares = 0
+
+for numero in tupla:
+    if numero % 2 == 0:
+        numerosPares += 1
+        pass
+    pass
+
+quantidade9 = tupla.count(9)
+
+print(f"Você digitou os valores: {tupla}")
+print(f"O valor 9 apareceu {quantidade9} vezes")
+if tupla.count(3) > 0:
+    posicao3 = tupla.index(3) + 1
+    print(f"O valor 3 apareceu na {posicao3}° posição")
+else:
+    print("O valor 3 não apareceu nenhuma vez")
+print(f"Os valores pares digitados foram {numerosPares}")
